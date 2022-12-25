888f9eb9a77fb49b5d796834348e55b601db4e52
Ujjawal Modanwal <51051403+ujjawalmodanwal@users.noreply.github.com>
2022-12-26 00:29:24 +0530

    updated

--- index.js
+++ index.js
@@ -11,16 +11,7 @@ connectDB();
 app.use(express.json());
 const PORT = process.env.PORT;
 
-var cors = require(cors());
 app.use(cors());
-app.options('*',cors());
-var allowCrossDomain = function(req,res,next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();  
-}
-app.use(allowCrossDomain);
 
 
 app.get('/', (req, res) => {
